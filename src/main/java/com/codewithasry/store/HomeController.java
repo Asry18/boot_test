71fa2430670fccce9a9f049a198ce8e25486788c
Asry18 <asryamaz000@gmail.com>
2025-11-01 21:53:44 +0530

    working backend

--- src/main/java/com/codewithasry/store/HomeController.java
+++ src/main/java/com/codewithasry/store/HomeController.java
@@ -1,12 +1,12 @@
 package com.codewithasry.store;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
-        return "index.html";
+        return "Store backend is running";
     }
 }
